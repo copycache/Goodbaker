8b9e40f1d9aa34c6de91dc2bcc9ad72059782e21
Andre Austria <andreaustria123@gmail.com>
2026-03-21 13:30:54 +0800

    v1.0

--- resources/js/composables/permission.js
+++
@@ -1,8 +0,0 @@
-import { usePage } from "@inertiajs/vue3";
-
-export function usePermission () {
-    const hasRole = (name) =>usePage().props.auth.user.roles.includes(name);
-    const hasPermission = (name) =>usePage().props.auth.user.includes(name);
-
-    return { hasRole, hasPermission };
-}
