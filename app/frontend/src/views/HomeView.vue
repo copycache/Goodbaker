7a184aae5ccd0ce29a795a7b1941c8fecb733b4b
Andre Austria <andreaustria123@gmail.com>
2026-03-22 14:47:53 +0800

    added shadui-vue

--- app/frontend/src/views/HomeView.vue
+++ app/frontend/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import Button from '../components/ui/button/Button.vue'
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <Button>Click me</Button>
   </main>
 </template>
